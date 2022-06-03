5bd1f598cd149889f19c9b2cd05c44a2475fcc01
Xinchao Song <git@xinchaosong.com>
2022-06-03 14:16:34 -0400

    Create the code to adapt acronym

--- scripts/generate_urdf.py
+++ scripts/generate_urdf.py
@@ -1,25 +1,59 @@
+import os
 from pathlib import Path
-
 import h5py
 
-data_path = Path(__file__).resolve().parent.parent / "data"
-acronym_path = data_path / "acronym" / "grasps"
-vgnized_dataset_path = data_path / "urdfs" / "acronym"
+acronym_path = "/media/DATACENTER2/HiLo_other_approaches/Acronym/"
+acronym_grasps_path = os.path.join(acronym_path, "grasps")
+
+urdfs_path = Path(__file__).resolve().parent.parent / "data" / "urdfs" / "acronym"
+urdfs_train_path = str(urdfs_path / "train")
+urdfs_val_path = str(urdfs_path / "val")
+urdfs_test_path = str(urdfs_path / "test")
 
 with open("template.urdf", "r") as f:
-    urdf_lines = f.readlines()
+    template_urdf_lines = f.readlines()
 
-grasps = h5py.File(acronym_path / 'WineGlass_2d89d2b3b6749a9d99fbba385cc0d41d_0.0024652679182251653.h5', 'r')
 
-obj_name = grasps['object/file'][()].decode('ascii')
-obj_name = obj_name[obj_name.rfind("/") + 1:]
-obj_mass = grasps['object/mass'][()]
+def generate_urdf(dataset, dataset_path):
+    for hdf5 in dataset:
+        hdf5_name = hdf5.strip()
+        grasps = h5py.File(os.path.join(acronym_grasps_path, hdf5_name), 'r')
 
-for i in range(len(urdf_lines)):
-    urdf_lines[i] = urdf_lines[i].replace("obj_name.obj", obj_name)
-    urdf_lines[i] = urdf_lines[i].replace("obj_mass", str(obj_mass))
+        obj_name = grasps['object/file'][()].decode('ascii')
+        obj_name = obj_name[obj_name.rfind("/") + 1:obj_name.rfind(".")]
+        obj_mass = grasps['object/mass'][()]
 
-urdf_name = obj_name[:-4] + ".urdf"
+        urdf_lines = template_urdf_lines.copy()
+        for i in range(len(urdf_lines)):
+            urdf_lines[i] = urdf_lines[i].replace("obj_name", obj_name)
+            urdf_lines[i] = urdf_lines[i].replace("obj_mass", str(obj_mass))
 
-with open(vgnized_dataset_path / urdf_name, "w") as f:
-    f.writelines(urdf_lines)
+        urdf_name = hdf5_name[:hdf5_name.rfind(".") + 1:] + ".urdf"
+
+        with open(os.path.join(dataset_path, urdf_name), "w") as f:
+            f.writelines(urdf_lines)
+
+
+with open(os.path.join(acronym_path, "train_set.txt"), "r") as f:
+    train_set = f.readlines()
+
+if not os.path.exists(urdfs_train_path):
+    os.mkdir(urdfs_train_path)
+
+generate_urdf(train_set, urdfs_train_path)
+
+with open(os.path.join(acronym_path, "val_set.txt"), "r") as f:
+    val_set = f.readlines()
+
+if not os.path.exists(urdfs_val_path):
+    os.mkdir(urdfs_val_path)
+
+generate_urdf(val_set, urdfs_val_path)
+
+with open(os.path.join(acronym_path, "test_set.txt"), "r") as f:
+    test_set = f.readlines()
+
+if not os.path.exists(urdfs_test_path):
+    os.mkdir(urdfs_test_path)
+
+generate_urdf(test_set, urdfs_test_path)
